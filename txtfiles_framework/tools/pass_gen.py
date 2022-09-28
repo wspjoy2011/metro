7f60955f01f781d892e7817f4fb903566587a597
wspjoy2011 <wspjoy2011@gmail.com>
2022-09-28 21:39:42 +0300

    Add text apps

--- txtfiles_framework/tools/pass_gen.py
+++ txtfiles_framework/tools/pass_gen.py
@@ -1,7 +1,8 @@
-from random import choice, shuffle, randint
+from random import choice, shuffle
 
 
 def generate_password_from_file(text):
+    """Generate password from random words from file"""
     letters = list(choice(text) + choice(text))
     shuffle(letters)
     letters_diff_case = ''.join(map(lambda x: x.upper() if choice((0, 1)) else x, letters))
