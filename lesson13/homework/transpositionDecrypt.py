feaf963569b7bb6efa0e91f06c36a95da94eff20
wspjoy2011 <wspjoy2011@gmail.com>
2022-10-21 18:46:40 +0300

    Add apps from lesson 13

--- lesson13/homework/transpositionDecrypt.py
+++ lesson13/homework/transpositionDecrypt.py
@@ -4,45 +4,18 @@
 import math, pyperclip
 
 
-def main():
-    myMessage = '''Ainlm75gti rrsn-uleo  dtn b   tpmuntab 2litk l ipty.nii  tcyAsrhueggeceN)icenweecuet  nsaaea sheetru-sero siroosaarriHtc slnra eg e.sNs  vwha,wr rlp,cehlr gdr a afrto (1ea n nkBl o areuetoeim iri aeo18msm c  aygstl  dchrdearsdrp lf01b aahfob eeh neeoei dce esrA,  5eatnionbmn eEon g tt hsodtod LD rnhder aeec ntgwnfhooiuf  gaCoe-  e f CgcroAgeihiim unltacr ovc 1Emwlhhehamnisnasr btetesoaKuee28naryea'alpan etesie .,n mm'''
-    myKey = 13
-
-    plaintext = decryptMessage(myKey, myMessage)
-
-    # Print with a | ("pipe" character) after it in case
-    # there are spaces at the end of the decrypted message.
-    print(plaintext + '|')
-
-    pyperclip.copy(plaintext)
-
-
 def decryptMessage(key, message):
-    # The transposition decrypt function will simulate the "columns" and
-    # "rows" of the grid that the plaintext is written on by using a list
-    # of strings. First, we need to calculate a few values.
-
-    # The number of "columns" in our transposition grid:
-    numOfColumns = int(math.ceil(len(message) / float(key)))
-    # The number of "rows" in our grid will need:
+    numOfColumns = int(math.ceil(len(message) / key))
     numOfRows = key
-    # The number of "shaded boxes" in the last "column" of the grid:
-    numOfShadedBoxes = (numOfColumns * numOfRows) - len(message)
-
-    # Each string in plaintext represents a column in the grid.
+    numOfShadedBoxes = numOfColumns * numOfRows - len(message)
     plaintext = [''] * numOfColumns
 
-    # The column and row variables point to where in the grid the next
-    # character in the encrypted message will go.
     column = 0
     row = 0
 
     for symbol in message:
         plaintext[column] += symbol
-        column += 1 # Point to next column.
-
-        # If there are no more columns OR we're at a shaded box, go back to
-        # the first column and the next row:
+        column += 1
         if (column == numOfColumns) or (column == numOfColumns - 1 and row >= numOfRows - numOfShadedBoxes):
             column = 0
             row += 1
@@ -50,7 +23,13 @@ def decryptMessage(key, message):
     return ''.join(plaintext)
 
 
-# If transpositionDecrypt.py is run (instead of imported as a module) call
-# the main() function.
+def main():
+    plaintext = decryptMessage(myKey, myMessage)
+    pyperclip.copy(plaintext)
+    return plaintext
+
+
 if __name__ == '__main__':
-    main()
+    myMessage = '''Ainlm75gti rrsn-uleo  dtn b   tpmuntab 2litk l ipty.nii  tcyAsrhueggeceN)icenweecuet  nsaaea sheetru-sero siroosaarriHtc slnra eg e.sNs  vwha,wr rlp,cehlr gdr a afrto (1ea n nkBl o areuetoeim iri aeo18msm c  aygstl  dchrdearsdrp lf01b aahfob eeh neeoei dce esrA,  5eatnionbmn eEon g tt hsodtod LD rnhder aeec ntgwnfhooiuf  gaCoe-  e f CgcroAgeihiim unltacr ovc 1Emwlhhehamnisnasr btetesoaKuee28naryea'alpan etesie .,n mm'''
+    myKey = 13
+    print(main())
--- lesson13/homework/transpositionEncrypt.py
+++ lesson13/homework/transpositionEncrypt.py
@@ -1,46 +1,25 @@
-# Transposition Cipher Encryption
-# https://www.nostarch.com/crackingcodes (BSD Licensed)
-
 import pyperclip
 
 
-def main():
-    my_message = """Augusta Ada King-Noel, Countess of Lovelace (10 December 1815 - 27 November 1852) was an English mathematician and writer, chiefly known for her work on Charles Babbage's early mechanical general-purpose computer, the Analytical Engine. Her notes on the engine include what is recognised as the first algorithm intended to be carried out by a machine. As a result, she is often regarded as the first computer programmer."""
-    myKey = 13
-    my_message = my_message
-    ciphertext = encrypt_message(myKey, my_message)
-
-    # Print the encrypted string in ciphertext to the screen, with
-    # a | ("pipe" character) after it in case there are spaces at
-    # the end of the encrypted message.
-    print(ciphertext + '|')
-
-    # Copy the encrypted string in ciphertext to the clipboard.
-    pyperclip.copy(ciphertext)
-
-
 def encrypt_message(key, message):
-    # Each string in ciphertext represents a column in the grid.
     ciphertext = [''] * key
-
-    # Loop through each column in ciphertext.
     for column in range(key):
         current_index = column
 
-        # Keep looping until current_index goes past the message length.
         while current_index < len(message):
-            # Place the character at current_index in message at the
-            # end of the current column in the ciphertext list.
             ciphertext[column] += message[current_index]
-
-            # move current_index over
             current_index += key
 
-    # Convert the ciphertext list into a single string value and return it.
     return ''.join(ciphertext)
 
 
-# If transpositionEncrypt.py is run (instead of imported as a module) call
-# the main() function.
+def main(message: str, key: int):
+    ciphertext = encrypt_message(my_key, my_message)
+    pyperclip.copy(ciphertext)
+    return ciphertext
+
+
 if __name__ == '__main__':
-    main()
+    my_message = """Augusta Ada King-Noel, Countess of Lovelace (10 December 1815 - 27 November 1852) was an English mathematician and writer, chiefly known for her work on Charles Babbage's early mechanical general-purpose computer, the Analytical Engine. Her notes on the engine include what is recognised as the first algorithm intended to be carried out by a machine. As a result, she is often regarded as the first computer programmer."""
+    my_key = 13
+    print(main(my_message, my_key))
